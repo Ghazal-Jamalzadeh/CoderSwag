be173e35cee1a3972e3975e8dd3b5689ff6be145
Your Name <jmzd.developer94@gmail.com>
2021-06-16 15:35:44 +0430

    List View

--- app/src/main/java/com/jmzd/ghazal/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/coderswag/controller/MainActivity.kt
@@ -2,11 +2,28 @@ package com.jmzd.ghazal.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.jmzd.ghazal.coderswag.R
+import com.jmzd.ghazal.coderswag.databinding.ActivityMainBinding
+import com.jmzd.ghazal.coderswag.model.Category
+import com.jmzd.ghazal.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories
+        ) // context , layout , data
+        binding.categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/jmzd/ghazal/coderswag/model/Category.kt
+++ app/src/main/java/com/jmzd/ghazal/coderswag/model/Category.kt
@@ -1,3 +1,8 @@
 package com.jmzd.ghazal.coderswag.model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+   // قبل از اد کردن این متد لیست را به صورت string نمایش نمیدهد داخل listView
+    override fun toString(): String {
+        return title
+    }
+}
