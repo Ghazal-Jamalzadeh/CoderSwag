50794585e456df7f7fc1c206a746c82133c0b67b
Your Name <jmzd.developer94@gmail.com>
2021-06-16 17:18:21 +0430

    View Holder

--- app/src/main/java/com/jmzd/ghazal/coderswag/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/jmzd/ghazal/coderswag/adapters/CategoryAdapter.kt
@@ -18,19 +18,29 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
+        val holder : ViewHolder
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
-        println("Heavy Computing")
+        if (convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+            categoryView.tag = holder
+            println("I exist for the first time...")
+        } else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+            //reusing the cells
+            println("go green , recycle ")
+        }
+// تو لیست ویو برای هر ردیف یک بارها و هربار این فرایند تکرار می‌شود و اصلا بهینه نیست. راه حل : ویو هلدر است که ویوهای تکراری را ریسایکل میکند و از اول نمیسازد و Heavy Computing اتفاق نمی افتد. بدون استفاده از ویوهلدر هم لیست ویو به خوبی کار می کند ولی در بک گراند پردازش سنگین صورت می گیرد.
 
         val category = categories[position]
-
         //convert image name to resource id
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
-        categoryName.text = category.title
+        holder.categoryImage?.setImageResource(resourceId)
+        holder.categoryName?.text = category.title
+
         return categoryView
     }
 
@@ -38,12 +48,18 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
         return categories[position]
     }
 
+    // this is a method that defines a unique ID for each row. but we don't need it for now.
     override fun getItemId(position: Int): Long {
         return 0
-        // this is a method that defines a unique ID for each row. but we don't need it for now.
     }
 
     override fun getCount(): Int {
         return categories.count()
+    }
+
+    // this view holder is going to hold a reference to our categoryImage & categoryName
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 }
